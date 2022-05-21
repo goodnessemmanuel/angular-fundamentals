754fbd67aacd898e96c4afc5954e214090786d6e
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-21 09:48:15 +0200

    add template variable logic

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input } from "@angular/core";
-
+import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: "event-thumbnail",
-    templateUrl: "./event-thumbnail.component.html"
+  selector: 'event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
 })
-export class EventThumbnailComponent{
-   @Input() event: any
+export class EventThumbnailComponent {
+  @Input() event: any;
+
+  public logFoo() {
+    console.log('Foo!!!');
+  }
 }
