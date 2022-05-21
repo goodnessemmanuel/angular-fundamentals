4feeebe70ba55c08e2af1cddf58bdd3b68dd4e42
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-21 10:27:48 +0200

    add nav component"

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "nav-component",
+    templateUrl: "./nav.component.html",
+    styles: [`
+    
+    `]
+})
+export class NavComponent{
+
+}
