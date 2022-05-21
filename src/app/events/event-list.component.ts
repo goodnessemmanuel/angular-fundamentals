24f52a2c9c4699468c5e3fe3613d9ce76a4fccfd
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-22 01:30:14 +0200

    integrate non-testable toaster feature

--- src/app/events/event-list.component.ts
+++ src/app/events/event-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
 
+  //This tells typescript that this variable is in scope already 
+  // and have been declared somewhere else
+  declare let toastr: any; //it is available globally
+  
 @Component({
   selector: 'event-list',
   templateUrl: './event-list.component.html',
@@ -9,8 +13,14 @@ export class EventListComponent implements OnInit {
   public events: any[] = [];  
 
   constructor(private eventService: EventService) {}
-  
+
   ngOnInit(): void {
       this.events = this.eventService.getEvents();
+  }
+
+  public handleThumbnailClick(eventName: string){
+        //alert("Event " + eventName)
+        //toastr.success(eventName);
+        toastr.info(eventName)
   }
 }
