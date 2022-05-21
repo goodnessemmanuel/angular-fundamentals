e7eacb0aee9508e863c8aa2b8c45632ddcacef99
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-21 22:44:18 +0200

    bind class or style

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,8 +8,16 @@ import { Component, Input } from "@angular/core";
         .thumbnail{ min-height: 210px }
         .padding-left{ margin-left: 10px; }
         .well div{ color: #bbb }
+        .bold{font-weight: bold}
+        .green{color: green !important}
     `]
 })
 export class EventThumbnailComponent{
    @Input() event: any
+
+   public getStartTimeClasses(): any[]{
+       if(this.event && this.event.time === '8:00 am')
+            return ["bold", "green"];
+        return []    
+   }
 }
