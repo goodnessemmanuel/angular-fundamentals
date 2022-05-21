f1dc745df501cd9bcfa8f8b558ee5f2ad2f013b6
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-21 13:10:07 +0200

    add safe navigation operator, style component

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,7 +3,12 @@ import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "event-thumbnail",
-    templateUrl: "./event-thumbnail.component.html"
+    templateUrl: "./event-thumbnail.component.html",
+    styles: [`
+        .thumbnail{ min-height: 210px }
+        .padding-left{ margin-left: 10px; }
+        .well div{ color: #bbb }
+    `]
 })
 export class EventThumbnailComponent{
    @Input() event: any
