d7ec96fb66b0a8de68c8e4a8499b025a85d791a0
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-22 00:52:55 +0200

    add event service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventAppComponent } from './event-app.component';
 import { EventListComponent } from './events/event-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavComponent } from './nav/nav.component';
+import { EventService } from './shared/event.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { NavComponent } from './nav/nav.component';
   imports: [
     BrowserModule
   ],
+  providers: [EventService],
   bootstrap: [EventAppComponent]
 })
 export class AppModule { }
