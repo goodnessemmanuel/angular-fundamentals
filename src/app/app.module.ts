afa5227620370d77cbb2a077b4700b839600b8cc
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-22 08:41:24 +0200

    implement canActivate route guard with a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from 'src/routes';
 import { ToasterService } from './common/toaster.service';
+import { Error404Component } from './errors/404.component';
 
 import { EventAppComponent } from './event-app.component';
+import { EventRouteActivatorService } from './events/even-route-activator.service';
 import { CreateEventComponent } from './events/event-create.component';
 import { EventDetailComponent } from './events/event-detail.component';
 import { EventListComponent } from './events/event-list.component';
@@ -19,13 +21,18 @@ import { EventService } from './shared/event.service';
     EventThumbnailComponent,
     NavComponent,
     EventDetailComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [EventService, ToasterService],
+  providers: [
+    EventService, 
+    ToasterService,
+    EventRouteActivatorService
+  ],
   bootstrap: [EventAppComponent]
 })
 export class AppModule { }
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@
  * intellisense and angular compile time safety
  */
 import { Routes } from '@angular/router';
+import { Error404Component } from './app/errors/404.component';
+import { EventRouteActivatorService } from './app/events/even-route-activator.service';
 import { CreateEventComponent } from './app/events/event-create.component';
 
 import { EventDetailComponent } from './app/events/event-detail.component';
@@ -19,6 +21,7 @@ import { EventListComponent } from './app/events/event-list.component';
 export const appRoutes: Routes = [
   { path: 'events/new', component: CreateEventComponent },
   { path: 'events', component: EventListComponent },
-  { path: 'events/:id', component: EventDetailComponent },
+  { path: 'events/:id', component: EventDetailComponent, canActivate: [EventRouteActivatorService] },
+  { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
 ];
